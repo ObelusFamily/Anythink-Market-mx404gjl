7957e7b0a3f428f9d84c0f4734ae1c4f31516de0
vanessa-cooper-1 <ness+1@anythink.family>
2023-01-23 12:08:51 +0000

    Navigating the Unknown

--- frontend/src/components/404.js
+++
@@ -1,8 +0,0 @@
-export default function NotFound() {
-    return (
-        <div>
-            <h1>Page not found</h1>
-            <p>Oops, Looks like you've stumbled upon an item that currently does not exist. But don't worry, there are plenty of other cool items to choose from in the Anythink marketplace. Take a look around and find something new!</p>
-        </div>
-    )
-}
